d07574f99076b9fba684e16494b075e6508230f3
hamza faouri <eng.hfaouri@gmail.com>
2024-11-27 21:14:07 +0100

    Refactor Terraform configuration: replace module with direct resource definition for Proxmox VM and remove unused module files

--- home-lab/terraform/provider.tf
+++ home-lab/terraform/provider.tf
@@ -2,17 +2,29 @@
 # ---
 # Initial Provider Configuration for Proxmox
 
+# terraform {
+
+#   required_version = ">= 0.13.0"
+
+#   required_providers {
+#     proxmox = {
+#       source  = "telmate/proxmox"
+#       version = "3.0.1-rc4"
+#     }
+#   }
+# }
+
+
 terraform {
-
   required_version = ">= 0.13.0"
-
   required_providers {
     proxmox = {
-      source  = "telmate/proxmox"
+      source = "Telmate/proxmox"
       version = "3.0.1-rc4"
     }
   }
 }
+
 
 provider "proxmox" {
 
